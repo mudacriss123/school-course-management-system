3b9831911a4928a4c310f5d3d0a9e337ba0a81f1
mudacriss123 <mc_mudassir@Hotmail.com>
2016-11-06 06:20:24 -0500

    Adminpage.php

---
+++ Adminpage.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        <h1>Student Management center</h1>
+        <br><br>
+        <form>
+        <input type="button" onclick="window.location.href='Addcourse.php'" value="Add Course"/> 
+        <input type="button" onclick="window.location.href='Deletecourse.php'" value="Delete Course"/>
+        </form>
+    </body>
+</html>
